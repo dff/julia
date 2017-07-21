00b84d228d14158a1aced37f8c96787b82382c36
kpamnany <kiran.pamnany@intel.com>
2017-07-21 14:39:43 -0400

    Moved multiq and synctreepool code into partr.c

--- src/partr.c
+++ src/partr.c
@@ -31,25 +31,333 @@ extern "C" {
 #ifdef JULIA_ENABLE_THREADING
 #ifdef JULIA_ENABLE_PARTR
 
-// multiq functions
-void multiq_init();
-int multiq_insert(jl_ptask_t *elem, int16_t priority);
-jl_ptask_t *multiq_deletemin();
+
+// multiq
+// ---
+
+/* a task heap */
+typedef struct taskheap_tag {
+    jl_mutex_t lock;
+    jl_ptask_t **tasks;
+    int16_t ntasks, prio;
+} taskheap_t;
+
+static const int16_t heap_d = 8;
+static const int heap_c = 4;
+static const int tasks_per_heap = 129;
+
+static taskheap_t *heaps;
+static int16_t heap_p;
+
+/* unbias state for the RNG */
+static uint64_t cong_unbias;
+
+
+/*  multiq_init()
+ */
+static inline void multiq_init()
+{
+    heap_p = heap_c * jl_n_threads;
+    heaps = (taskheap_t *)calloc(heap_p, sizeof(taskheap_t));
+    for (int16_t i = 0;  i < heap_p;  ++i) {
+        jl_mutex_init(&heaps[i].lock);
+        heaps[i].tasks = (jl_ptask_t **)calloc(tasks_per_heap, sizeof(jl_ptask_t *));
+        heaps[i].ntasks = 0;
+        heaps[i].prio = INT16_MAX;
+    }
+    unbias_cong(heap_p, &cong_unbias);
+}
+
+
+/*  sift_up()
+ */
+static inline void sift_up(taskheap_t *heap, int16_t idx)
+{
+    if (idx > 0) {
+        int16_t parent = (idx-1)/heap_d;
+        if (heap->tasks[idx]->prio <= heap->tasks[parent]->prio) {
+            jl_ptask_t *t = heap->tasks[parent];
+            heap->tasks[parent] = heap->tasks[idx];
+            heap->tasks[idx] = t;
+            sift_up(heap, parent);
+        }
+    }
+}
+
+
+/*  sift_down()
+ */
+static inline void sift_down(taskheap_t *heap, int16_t idx)
+{
+    if (idx < heap->ntasks) {
+        for (int16_t child = heap_d*idx + 1;
+                child < tasks_per_heap && child <= heap_d*idx + heap_d;
+                ++child) {
+            if (heap->tasks[child]
+                    &&  heap->tasks[child]->prio <= heap->tasks[idx]->prio) {
+                jl_ptask_t *t = heap->tasks[idx];
+                heap->tasks[idx] = heap->tasks[child];
+                heap->tasks[child] = t;
+                sift_down(heap, child);
+            }
+        }
+    }
+}
+
+
+/*  multiq_insert()
+ */
+static inline int multiq_insert(jl_ptask_t *task, int16_t priority)
+{
+    jl_ptls_t ptls = jl_get_ptls_states();
+    uint64_t rn;
+
+    task->prio = priority;
+    do {
+        rn = cong(heap_p, cong_unbias, &ptls->rngseed);
+    } while (!jl_mutex_trylock_nogc(&heaps[rn].lock));
+
+    if (heaps[rn].ntasks >= tasks_per_heap) {
+        jl_mutex_unlock_nogc(&heaps[rn].lock);
+        return -1;
+    }
+
+    heaps[rn].tasks[heaps[rn].ntasks++] = task;
+    sift_up(&heaps[rn], heaps[rn].ntasks-1);
+    jl_mutex_unlock_nogc(&heaps[rn].lock);
+    int16_t prio = jl_atomic_load(&heaps[rn].prio);
+    if (task->prio < prio)
+        jl_atomic_compare_exchange(&heaps[rn].prio, prio, task->prio);
+
+    return 0;
+}
+
+
+/*  multiq_deletemin()
+ */
+static inline jl_ptask_t *multiq_deletemin()
+{
+    jl_ptls_t ptls = jl_get_ptls_states();
+    uint64_t rn1, rn2;
+    int16_t i, prio1, prio2;
+    jl_ptask_t *task;
+
+    for (i = 0;  i < jl_n_threads;  ++i) {
+        rn1 = cong(heap_p, cong_unbias, &ptls->rngseed);
+        rn2 = cong(heap_p, cong_unbias, &ptls->rngseed);
+        prio1 = jl_atomic_load(&heaps[rn1].prio);
+        prio2 = jl_atomic_load(&heaps[rn2].prio);
+        if (prio1 > prio2) {
+            prio1 = prio2;
+            rn1 = rn2;
+        }
+        else if (prio1 == prio2 && prio1 == INT16_MAX)
+            continue;
+        if (jl_mutex_trylock_nogc(&heaps[rn1].lock)) {
+            if (prio1 == heaps[rn1].prio)
+                break;
+            jl_mutex_unlock_nogc(&heaps[rn1].lock);
+        }
+    }
+    if (i == jl_n_threads)
+        return NULL;
+
+    task = heaps[rn1].tasks[0];
+    heaps[rn1].tasks[0] = heaps[rn1].tasks[--heaps[rn1].ntasks];
+    heaps[rn1].tasks[heaps[rn1].ntasks] = NULL;
+    prio1 = INT16_MAX;
+    if (heaps[rn1].ntasks > 0) {
+        sift_down(&heaps[rn1], 0);
+        prio1 = heaps[rn1].tasks[0]->prio;
+    }
+    jl_atomic_store(&heaps[rn1].prio, prio1);
+    jl_mutex_unlock_nogc(&heaps[rn1].lock);
+
+    return task;
+}
+
 
 // sync trees
-void synctreepool_init();
-arriver_t *arriver_alloc();
-void arriver_free(arriver_t *);
-reducer_t *reducer_alloc();
-void reducer_free(reducer_t *);
+// ---
 
-int last_arriver(arriver_t *, int);
-void *reduce(arriver_t *, reducer_t *, void *(*rf)(void *, void *), void *, int);
+/* arrival tree */
+struct _arriver_t {
+    int16_t index, next_avail;
+    int16_t **tree;
+};
+
+/* reduction tree */
+struct _reducer_t {
+    int16_t index, next_avail;
+    void ***tree;
+};
+
+
+/* pool of arrival trees */
+static arriver_t *arriverpool;
+static int16_t num_arrivers, num_arriver_tree_nodes, next_arriver;
+
+/* pool of reduction trees */
+static reducer_t *reducerpool;
+static int16_t num_reducers, num_reducer_tree_nodes, next_reducer;
+
+
+/*  synctreepool_init()
+ */
+static inline void synctreepool_init()
+{
+    num_arriver_tree_nodes = (GRAIN_K * jl_n_threads) - 1;
+    num_reducer_tree_nodes = (2 * GRAIN_K * jl_n_threads) - 1;
+
+    /* num_arrivers = ((GRAIN_K * jl_n_threads) ^ ARRIVERS_P) + 1 */
+    num_arrivers = GRAIN_K * jl_n_threads;
+    for (int i = 1;  i < ARRIVERS_P;  ++i)
+        num_arrivers = num_arrivers * num_arrivers;
+    ++num_arrivers;
+
+    num_reducers = num_arrivers * REDUCERS_FRAC;
+
+    /* allocate */
+    arriverpool = (arriver_t *)calloc(num_arrivers, sizeof (arriver_t));
+    next_arriver = 0;
+    for (int i = 0;  i < num_arrivers;  ++i) {
+        arriverpool[i].index = i;
+        arriverpool[i].next_avail = i + 1;
+        arriverpool[i].tree = (int16_t **)
+                jl_malloc_aligned(num_arriver_tree_nodes * sizeof (int16_t *), 64);
+        for (int j = 0;  j < num_arriver_tree_nodes;  ++j)
+            arriverpool[i].tree[j] = (int16_t *)jl_malloc_aligned(sizeof (int16_t), 64);
+    }
+    arriverpool[num_arrivers - 1].next_avail = -1;
+
+    reducerpool = (reducer_t *)calloc(num_reducers, sizeof (reducer_t));
+    next_reducer = 0;
+    for (int i = 0;  i < num_reducers;  ++i) {
+        reducerpool[i].index = i;
+        reducerpool[i].next_avail = i + 1;
+        reducerpool[i].tree = (void ***)
+                jl_malloc_aligned(num_reducer_tree_nodes * sizeof (void **), 64);
+        for (int j = 0;  j < num_reducer_tree_nodes;  ++j)
+            reducerpool[i].tree[j] = (void **)jl_malloc_aligned(sizeof (void *), 64);
+    }
+    if (num_reducers > 0)
+        reducerpool[num_reducers - 1].next_avail = -1;
+    else
+        next_reducer = -1;
+}
+
+
+/*  arriver_alloc()
+ */
+static inline arriver_t *arriver_alloc()
+{
+    int16_t candidate;
+    arriver_t *arr;
+
+    do {
+        candidate = jl_atomic_load(&next_arriver);
+        if (candidate == -1)
+            return NULL;
+        arr = &arriverpool[candidate];
+    } while (!jl_atomic_bool_compare_exchange(&next_arriver,
+                candidate, arr->next_avail));
+    return arr;
+}
+
+
+/*  arriver_free()
+ */
+static inline void arriver_free(arriver_t *arr)
+{
+    for (int i = 0;  i < num_arriver_tree_nodes;  ++i)
+        *arr->tree[i] = 0;
+
+    jl_atomic_exchange_generic(&next_arriver, &arr->index, &arr->next_avail);
+}
+
+
+/*  reducer_alloc()
+ */
+static inline reducer_t *reducer_alloc()
+{
+    int16_t candidate;
+    reducer_t *red;
+
+    do {
+        candidate = jl_atomic_load(&next_reducer);
+        if (candidate == -1)
+            return NULL;
+        red = &reducerpool[candidate];
+    } while (!jl_atomic_bool_compare_exchange(&next_reducer,
+                     candidate, red->next_avail));
+    return red;
+}
+
+
+/*  reducer_free()
+ */
+static inline void reducer_free(reducer_t *red)
+{
+    for (int i = 0;  i < num_reducer_tree_nodes;  ++i)
+        *red->tree[i] = 0;
+
+    jl_atomic_exchange_generic(&next_reducer, &red->index, &red->next_avail);
+}
+
+
+/*  last_arriver()
+ */
+static inline int last_arriver(arriver_t *arr, int idx)
+{
+    int arrived, aidx = idx + (GRAIN_K * jl_n_threads) - 1;
+
+    while (aidx > 0) {
+        --aidx;
+        aidx >>= 1;
+        arrived = jl_atomic_fetch_add(arr->tree[aidx], 1);
+        if (!arrived) return 0;
+    }
+
+    return 1;
+}
+
+
+/*  reduce()
+ */
+static inline void *reduce(arriver_t *arr, reducer_t *red, void *(*rf)(void *, void *),
+                           void *val, int idx)
+{
+    int arrived, aidx = idx + (GRAIN_K * jl_n_threads) - 1, ridx = aidx, nidx;
+
+    *red->tree[ridx] = val;
+    while (aidx > 0) {
+        --aidx;
+        aidx >>= 1;
+        arrived = jl_atomic_fetch_add(arr->tree[aidx], 1);
+        if (!arrived) return NULL;
+
+        /* neighbor has already arrived, get its value and reduce it */
+        nidx = ridx & 0x1 ? ridx + 1 : ridx - 1;
+        val = rf(val, *red->tree[nidx]);
+
+        /* move up the tree */
+        --ridx;
+        ridx >>= 1;
+        *red->tree[ridx] = val;
+    }
+
+    return val;
+}
+
+
+// parallel task runtime
+// ---
 
 // sticky task queues need to be visible to all threads
 jl_ptaskq_t *sticky_taskqs;
 
-/* internally used to indicate a yield occurred in the runtime itself */
+// internally used to indicate a yield occurred in the runtime itself
+// TODO: what's the Julia way to do this? A symbol?
 static const int64_t yield_from_sync = 1;
 
 
@@ -124,16 +432,6 @@ void jl_threadfun(void *arg)
     /* get the highest priority task and run it */
     while (run_next() == 0)
         ;
-}
-
-
-// old threading interface: run specified function in all threads. partr creates
-// jl_n_threads tasks and enqueues them; these may not actually run in all the
-// threads.
-JL_DLLEXPORT jl_value_t *jl_threading_run(jl_value_t *_args)
-{
-    // TODO.
-    return NULL;
 }
 
 
@@ -223,9 +521,22 @@ static jl_ptask_t *get_from_stickyq()
 }
 
 
-static int64_t resume(jl_ptask_t *task)
+// start the task if it is new, or switch to it
+static jl_value_t *resume(jl_ptask_t *task)
 {
     jl_ptls_t ptls = jl_get_ptls_states();
+
+    // GC safe
+    uint32_t nargs;
+    jl_value_t **args;
+    if (!jl_is_svec(task->args)) {
+        nargs = 1;
+        args = &task->args;
+    }
+    else {
+        nargs = jl_svec_len(task->args);
+        args = jl_svec_data(task->args);
+    }
 
     // TODO: before we support getting return value from
     //       the work, and after we have proper GC transition
@@ -233,14 +544,20 @@ static int64_t resume(jl_ptask_t *task)
     //       enter GC unsafe region when starting the work.
     int8_t gc_state = jl_gc_unsafe_enter(ptls);
 
-    if (task->started) {
+    jl_value_t *result = NULL;
+    if (!task->started) {
+        task->started = 1;
+        result = task->result = jl_thread_run_fun(&task->fptr, task->mfunc, args, nargs);
     }
     else {
+        // TODO: switch to task
+    }
 
     jl_gc_unsafe_leave(ptls, gc_state);
 
-    return y;
+    return result;
 }
+
 
 // get the next available task and run it
 static int run_next()
